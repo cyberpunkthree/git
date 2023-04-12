b5b9f8cfea437aa6262f334b9e32a195c643f135
Patrick Steinhardt <ps@pks.im>
2023-04-12 12:22:48 +0200

    pack-objects: fix error when same packfile is included and excluded
    When passing the same packfile both as included and excluded via the
    `--stdin-packs` option, then we will return an error because the
    excluded packfile cannot be found. This is because we will only set the
    `util` pointer for the included packfile list if it was found, so that
    we later die when we notice that it's in fact not set for the excluded
    packfile list.
    
    Fix this bug by always setting the `util` pointer for both the included
    and excluded list entries.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5331-pack-objects-stdin.sh
+++ t/t5331-pack-objects-stdin.sh
@@ -7,6 +7,12 @@ export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 TEST_PASSES_SANITIZE_LEAK=true
 . ./test-lib.sh
 
+packed_objects() {
+	git show-index <"$1" >tmp-object-list &&
+	cut -d' ' -f2 tmp-object-list &&
+	rm tmp-object-list
+ }
+
 test_expect_success 'pack-objects --stdin with duplicate packfile' '
 	test_when_finished "rm -fr repo" &&
 
@@ -23,6 +29,26 @@ test_expect_success 'pack-objects --stdin with duplicate packfile' '
 
 		git pack-objects --stdin-packs generated-pack <packfiles &&
 		test_cmp generated-pack-*.pack .git/objects/pack/pack-*.pack
+	)
+'
+
+test_expect_success 'pack-objects --stdin with same packfile excluded and included' '
+	test_when_finished "rm -fr repo" &&
+
+	git init repo &&
+	(
+		cd repo &&
+		test_commit "commit" &&
+		git repack -ad &&
+
+		(
+			basename .git/objects/pack/pack-*.pack &&
+			printf "^%s\n" "$(basename .git/objects/pack/pack-*.pack)"
+		) >packfiles &&
+
+		git pack-objects --stdin-packs generated-pack <packfiles &&
+		packed_objects generated-pack-*.idx >packed-objects &&
+		test_must_be_empty packed-objects
 	)
 '
 
